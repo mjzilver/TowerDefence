05c96bed8a8fb9127b07b94da87e208a4e3d2b40
mjzilver <4ilverj@gmail.com>
2025-12-08 23:36:41 +0100

    smaller entity

--- src/ecs/Component.h
+++ src/ecs/Component.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <cstdint>
 
-using Entity = std::uint32_t;
-const Entity INVALID_ENTITY = -1;
+using Entity = std::uint16_t;
+const Entity INVALID_ENTITY = UINT16_MAX;
 
 class Component {
 public:
